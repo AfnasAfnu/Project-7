8ca5c5fc724b133c0e5116fc67897cf4654de784
afnas <afnascode@gmail.com>
2023-05-14 00:10:01 +0530

    added view-addshow

--- school/school/urls.py
+++ school/school/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-
+from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('',views.add_show, name='addandshow'),
 ]
